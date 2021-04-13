36940cc46d9347cf32043cdd8764f60d3a1c44af
cheneysilent <58360026+cheneysilent@users.noreply.github.com>
2021-04-13 21:16:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,7 +36,7 @@ git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/che
 git clone -b master https://github.com/fw876/helloworld package/ssrplus
 
 # 常用插件包
-git clone https://github.com/kenzok8/openwrt-packages.git packages/openwrtpackages
+git clone https://github.com/kenzok8/openwrt-packages.git package/packagespls
 
 #passwall依赖包
 git clone https://github.com/kenzok8/small.git package/cheneysource/luci-app-small
